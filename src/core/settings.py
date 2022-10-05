bde8dc63e281e15db6cb1a680b9bd2fadbea0694
Robin Martens <robin.martens1@student.kuleuven.be>
2022-10-05 20:03:00 +0200

    Use MySQL-database

--- src/core/settings.py
+++ src/core/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = "src.core.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'USER': 'root',
+        'PASSWORD': getenv('DATABASE_PASSWORD'),
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
