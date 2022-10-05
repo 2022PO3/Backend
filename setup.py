63901cf24b0ae613802dca14ef6ddd3ead39b6c1
Robin Martens <robin.martens1@student.kuleuven.be>
2022-10-05 17:52:20 +0200

    Add GitHub workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="PO Backend API",
+        version="1.0",
+        description="The P&O3 Parking garage Backend.",
+        author="CW1B2",
+        packages=["api", "Backend"],
+    )
