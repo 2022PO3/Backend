a19c4fcb94c28376770558ba0ba98f7a7a56344f
Robin Martens <robin.martens1@student.kuleuven.be>
2022-10-05 18:17:41 +0200

    Rename Backend to core and alter `manage.py`

--- src/core/settings.py
+++ src/core/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "Backend.urls"
+ROOT_URLCONF = "src.core.urls"
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "Backend.wsgi.application"
+WSGI_APPLICATION = "src.core.wsgi.application"
 
 
 # Database
