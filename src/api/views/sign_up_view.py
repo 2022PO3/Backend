ccb26c3dcc33a0cfb96704a5530846ee7e3a483e
Robin Martens <robin.martens1@student.kuleuven.be>
2022-11-04 20:28:43 +0100

    Add sign up view

--- src/api/views/sign_up_view.py
+++ src/api/views/sign_up_view.py
@@ -1,10 +1,27 @@
+from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.request import Request
 
-from rest_framework.views import APIView
+from src.api.serializers.users_serializer import UsersSerializer
+from src.users.models import User
 
 
 class SignUpView(APIView):
     def post(self, request: Request, format=None) -> Response:
+        user_data = JSONParser().parse(request)
+        user_serializer = UsersSerializer(data=user_data)
+        if user_serializer.is_valid():
+            User.objects.create_user(
+                user_serializer["email"],
+                user_serializer["password"],
+                user_serializer["role"],
+            )
+            return Response(
+                {"data": user_serializer.data}, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {"errors": user_serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
