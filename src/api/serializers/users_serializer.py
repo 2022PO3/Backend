8630e1c9ae7294771bed420f38f2a8fa783a83db
Robin Martens <robin.martens1@student.kuleuven.be>
2022-11-08 14:38:10 +0100

    Remove unused import

--- src/api/serializers/users_serializer.py
+++ src/api/serializers/users_serializer.py
@@ -1,7 +1,5 @@
-from typing import Any
-
 from rest_framework import serializers
-from rest_framework.exceptions import ErrorDetail, ValidationError
+from rest_framework.exceptions import ValidationError
 from src.users.models import User
 from django.contrib.auth import authenticate
 
