34149bf42da543f8664a35da7aa6f571eedcd2df
Robin Martens <robin.martens1@student.kuleuven.be>
2022-11-25 15:24:33 +0000

    Alter debug settings to env variable

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(getenv("DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 
