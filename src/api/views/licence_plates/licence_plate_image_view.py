d0e9ec32400737f4d8c2e4e25021b4e6825da4e4
Robin Martens <robin.martens1@student.kuleuven.be>
2022-12-13 17:07:24 +0100

    Use alternative method for licence plates

--- src/api/views/licence_plates/licence_plate_image_view.py
+++ src/api/views/licence_plates/licence_plate_image_view.py
@@ -1,31 +1,26 @@
-import os
-import re
-import shutil
 from datetime import datetime
+from typing import Any
 
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.permissions import AllowAny
 
-from src.core.settings import DEBUG
 from src.core.utils.stripe_endpoints import send_invoice
 from src.core.views import BackendResponse, _OriginAPIView
 
 from src.core.utils import to_snake_case
 from src.core.views import BackendResponse, _OriginAPIView, parse_frontend_json
-from src.api.models import Image, LicencePlate
+from src.api.models import LicencePlate
 
-from anpr.license_plate_recognition import ANPR
-from anpr.google_vision_ocr import GoogleVisionOCR
 from src.users.models import User
 
 
 class LicencePlateImageView(_OriginAPIView):
-    #     """
-    #     A view class to handle the incoming images in base64-format. From within this view, a image
-    #     processing is performed and a request is sent to the Google Vision API. The image itself
-    #     will NOT be stored in the database, but deleted when the function ends.
-    #     """
+    """
+    A view class to handle the incoming images in base64-format. From within this view, a image
+    processing is performed and a request is sent to the Google Vision API. The image itself
+    will NOT be stored in the database, but deleted when the function ends.
+    """
 
     permission_classes = [AllowAny]
     origins = ["rpi"]
@@ -33,29 +28,19 @@ class LicencePlateImageView(_OriginAPIView):
     def post(self, request: Request, format=None) -> BackendResponse:
         if (resp := super().post(request, format)) is not None:
             return resp
-        file = request.data["file"]  # type: ignore
-        try:
-            garage_id: int = request.headers["PO3-GARAGE-ID"]
-        except KeyError:
-            return BackendResponse(
-                ["The PO3-GARAGE-ID-header is not sent with the request."],
-                status=status.HTTP_400_BAD_REQUEST,
+        data = parse_frontend_json(request)
+        serializer = PostLicencePlateSerializer(data=data)  # type: ignore
+        if serializer.is_valid():
+            if serializer.validated_data["licence_plate"] == "0AAA000":  # type: ignore
+                return BackendResponse("OK", status=status.HTTP_200_OK)
+            handle_licence_plate(
+                serializer.data,
             )
-        Image.objects.create(image=file)
-        lp = ""
-        try:
-            lp = perform_ocr(file.name)  # type: ignore
-        except Exception as e:
-            delete_file(DEBUG)
-            return BackendResponse(
-                [f"An error occurred while parsing the licence plate image: {e}."],
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
-        stripped_lp_string = strip_special_chars(lp)
-        response = handle_licence_plate(stripped_lp_string, garage_id)
-        delete_file(DEBUG)
-
-        return response
+            return BackendResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return BackendResponse(
+            [serializer.errors],  # type: ignore
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 def _register_licence_plate(licence_plate: str, garage_id: int) -> BackendResponse:
@@ -134,7 +119,7 @@ def _sign_out_licence_plate(licence_plate: LicencePlate) -> BackendResponse:
     )
 
 
-def handle_licence_plate(licence_plate: str, garage_id: int) -> BackendResponse:
+def handle_licence_plate(data: dict[str, Any]) -> BackendResponse:
     """
     This function handles the business logic for incoming licence plates.
 
@@ -146,6 +131,8 @@ def handle_licence_plate(licence_plate: str, garage_id: int) -> BackendResponse:
     The variable `params` contains the fields `garageId` and `licencePlate` from the
     `LicencePlateSerializer`.
     """
+    licence_plate: str = data["licence_plate"]
+    garage_id: int = data["garage_id"]
     queryset = LicencePlate.objects.filter(licence_plate=licence_plate)
     if not queryset:
         return _register_licence_plate(licence_plate, garage_id)
@@ -156,56 +143,3 @@ def handle_licence_plate(licence_plate: str, garage_id: int) -> BackendResponse:
             if lp.in_garage
             else _sign_out_licence_plate(lp)
         )
-
-
-def perform_ocr(image_path: str) -> str:
-    anpr = ANPR(None, GoogleVisionOCR(), formats=["N-LLL-NNN", "N:LLL-NNN", "LLL-NNN"], verbosity=0)  # type: ignore
-    ocr_results = anpr.find_and_ocr(
-        os.path.join(os.getcwd(), f"src/media/images/{image_path}")
-    )
-    if not ocr_results:
-        raise Exception("No licence plates detected on the image.")
-
-    lp = "".join([lp.text for lp in ocr_results])
-    return lp
-
-
-def strip_special_chars(string: str) -> str:
-    """
-    Strips out all special characters from a given string.
-    """
-    return re.sub(r"\W", "", string)
-
-
-def delete_file(debug: bool) -> None:
-    """
-    Delete the entire directory of the image files, which makes sure that none remain on the
-    server.
-    """
-    if not debug:
-        try:
-            shutil.rmtree(os.path.join(os.getcwd(), "src/media/images"))
-        except FileNotFoundError:
-            pass
-        # Delete record from the database.
-        try:
-            Image.objects.all().delete()
-        except Exception as e:
-            print("Image deletion exception:", e)
-
-    # def post(self, request: Request, format=None) -> BackendResponse:
-    #     if (resp := super().post(request, format)) is not None:
-    #        return resp
-    #     data = parse_frontend_json(request)
-    #     serializer = PostLicencePlateSerializer(data=data)  # type: ignore
-    #     if serializer.is_valid():
-    #         if serializer.validated_data["licence_plate"] == "0AAA-111":  # type: ignore
-    #             return BackendResponse("OK", status=status.HTTP_200_OK)
-    #         LicencePlate.handle_licence_plate(
-    #             serializer.data,
-    #         )
-    #         return BackendResponse(serializer.data, status=status.HTTP_201_CREATED)
-    #     return BackendResponse(
-    #         [serializer.errors],  # type: ignore
-    #         status=status.HTTP_400_BAD_REQUEST,
-    #     )
